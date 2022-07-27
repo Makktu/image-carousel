fb51d66b4157b98c7464a5dd4450b8fbed7185cd
Makktu <johnmcna@gmail.com>
2022-07-27 17:09:12 +0100

    basic functionality

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-"use strict";
+'use strict';
+
+function selectRandomImages(amountOfImagesRequired) {
+    let arrayOfImages = [];
+
+    for (let a = 0; a < amountOfImagesRequired; a++) {
+        arrayOfImages[a] = `<img src="https://source.unsplash.com/random">`;
+    }
+
+    return arrayOfImages;
+}
+
+const imageSpace = document.querySelector('.image-holder');
+
+const leftArrow = document.querySelector('.left-arrow');
+leftArrow.addEventListener('click', () => {
+    imageSpace.innerHTML = '<img src="https://source.unsplash.com/random" />';
+});
+
+const rightArrow = document.querySelector('.right-arrow');
+rightArrow.addEventListener('click', () => {
+    imageSpace.innerHTML = '<img src="https://source.unsplash.com/random" />';
+});
