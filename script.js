79b70f3abd9eef5ba8bb2ee28d49a3294a5b78ad
Makktu <johnmcna@gmail.com>
2022-07-29 01:38:16 +0100

    added main functions

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let currentImage = 4;
+let currentImage = 3;
 
 let imageArray = [
     'banana.jpg',
@@ -12,26 +12,64 @@ let imageArray = [
     'tv.jpg',
 ];
 
+function sortBreadcrumbs(currentImage) {
+    console.log(currentImage);
+    breadcrumbs.forEach((crumb) => {
+        crumb.classList.remove('active');
+    });
+    breadcrumbs[currentImage].classList.add('active');
+}
+
+let btnTimer;
+
 const imageSpace = document.querySelector('.image-holder');
+
+const breadcrumbs = document.querySelectorAll('.bc');
+
+breadcrumbs.forEach((crumb) => {
+    crumb.addEventListener('click', (e) => {});
+});
 
 imageSpace.innerHTML = `<img src='./img/${imageArray[currentImage]}'>`;
 
 const leftArrow = document.querySelector('.left-arrow');
 
 leftArrow.addEventListener('click', () => {
-    if ((currentImage = -1)) currentImage = 6;
+    clearTimeout(btnTimer);
+    if (currentImage !== 0) currentImage--;
+
     imageSpace.innerHTML = `<img src='./img/${imageArray[currentImage]}'>`;
-    currentImage--;
+
+    if (currentImage == -1) {
+        currentImage = 0;
+    }
+
+    sortBreadcrumbs(currentImage);
+    startTimer();
 });
 
 const rightArrow = document.querySelector('.right-arrow');
 
 rightArrow.addEventListener('click', () => {
-    if (currentImage == 7) {
-        currentImage = 0;
-    }
+    clearTimeout(btnTimer);
+    if (currentImage !== 6) currentImage++;
 
     imageSpace.innerHTML = `<img src='./img/${imageArray[currentImage]}'>`;
 
-    currentImage++;
+    sortBreadcrumbs(currentImage);
+    startTimer();
 });
+
+function notPressed() {
+    let pickedPic = Math.floor(Math.random() * 6);
+    if (pickedPic == currentImage) notPressed();
+    imageSpace.innerHTML = `<img src='./img/${imageArray[pickedPic]}'>`;
+    sortBreadcrumbs(pickedPic);
+    startTimer();
+}
+
+function startTimer() {
+    btnTimer = window.setTimeout(notPressed, 5000);
+}
+
+startTimer();
